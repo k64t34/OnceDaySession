cc9626b2cf06b3ab22c72b0c55555ed9837b6dd9
Andrey <k64t34@gmail.com>
2015-10-25 00:00:42 +0300

    24.10.2015

--- OnceDaySession/Program.cs
+++ OnceDaySession/Program.cs
@@ -7,7 +7,10 @@
  * To change this template use Tools | Options | Coding | Edit Standard Headers.
  */
 using System;
+using System.Threading;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Drawing;
 
 namespace OnceDaySession
 {
@@ -19,13 +22,64 @@ namespace OnceDaySession
 		/// <summary>
 		/// Program entry point.
 		/// </summary>
+		/// 
+		static int TimerInterval=2; //Timer in minutes 
+		static int CountdownTimerInterval=4; //Timer in minutes 
 		[STAThread]
+		
+		
 		private static void Main(string[] args)
 		{
-			Application.EnableVisualStyles();
-			Application.SetCompatibleTextRenderingDefault(false);
-			Application.Run(new MainForm());
+			Thread.Sleep(TimerInterval*1000);
+			Thread tAlarm1 = new Thread(ShowAlarm1);
+    		tAlarm1.Start();            
+			
+			Thread.Sleep(CountdownTimerInterval*1000);
+			Thread tAlarm2 = new Thread(ShowAlarm2);
+			tAlarm1.Abort();
+    		tAlarm2.Start();   
+			Thread.Sleep(1000);
+
+			//ExitWindows.LogOff(true);
+			
+				
+			Thread.Sleep(3000);
+			
+			tAlarm2.Abort();
 		}
 		
+		static  void ShowAlarm1() 
+		{
+		ShowAlarm("Ваш сеанс работы будет прекращен через "+CountdownTimerInterval+" сек."); 	
+		}
+		static void ShowAlarm2() 
+		{
+		ShowAlarm("Завершение сеанса"); 		
+		}
+		static void ShowAlarm(String msg) 
+		{
+		Form f = new Form();
+		
+		f.StartPosition=FormStartPosition.CenterScreen;
+		f.Width=200;
+		f.Height=100;
+			
+		//f.SetBounds(10,10,200,200);		
+		f.FormBorderStyle = FormBorderStyle.None;
+		
+		f.MinimizeBox = false;
+		f.MaximizeBox = false;
+		f.ShowInTaskbar = false;			
+		Label l = new Label();
+		l.Text=msg;		
+		l.TextAlign=ContentAlignment.MiddleCenter;
+		float currentSize;
+		currentSize = l.Font.Size;
+        currentSize += 4.0F;
+        l.Font = new Font(l.Font.Name, currentSize,l.Font.Style, l.Font.Unit);
+        l.Dock=DockStyle.Fill;
+		f.Controls.Add(l);			
+		f.ShowDialog();	
+		}
 	}
 }
